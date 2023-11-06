4c55966f990dd0dae3a1db6fb9b86235ad42c03c
Paul Morisse <paul.morisse@etu.unicaen.fr>
2023-11-06 14:06:56 +0100

    🐛 fix: correction d'un appel de méthode obsolète

--- templates/carteRecette.php
+++ templates/carteRecette.php
@@ -10,7 +10,7 @@ require_once('./src/model/recette.php');
 function afficher($recette){?>
     <div class="col">
     <div class="d-flex flex-column flex-lg-row">
-        <div class="w-100" style="max-width: initial;"><img class="rounded img-fluid d-block w-100 fit-cover" style="height: 200px;" src= "<?=$recette->getUrlImage()?>">
+        <div class="w-100" style="max-width: initial;"><img class="rounded img-fluid d-block w-100 fit-cover" style="height: 200px;" src= "<?=$recette->getImage()?>">
             <ul style="background: transparent;padding-left: 0;border: 2px none var(--bs-primary-text-emphasis) ;border-left-color: var(--bs-primary-text-emphasis);">
                 <li style="background: var(--bs-primary-border-subtle);border-radius: 54px;border-top-left-radius: 0;border-bottom-left-radius: 0;text-align: center;border-style: solid;border-color: var(--bs-primary);">tag 1</li>
                 <li style="background: var(--bs-primary-border-subtle);border-radius: 54px;border-top-left-radius: 0;border-bottom-left-radius: 0;text-align: center;border-style: solid;border-color: var(--bs-primary);">tag 1</li>
