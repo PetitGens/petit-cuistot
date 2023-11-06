62c9d8075e5f8f2ba9449f19567a813baa5175d9
victorfriboulet07 <victorfriboulet07@gmail.com>
2023-11-06 16:08:11 +0100

    :Sparkles: methode de recette par filtre dans recette manager

--- src/model/recetteManager.php
+++ src/model/recetteManager.php
@@ -34,4 +34,24 @@ class RecetteManager extends Manager {
 
         return $recettes;
     }
+    public function getRecettesFiltre(string $Filtre,string $VALUE): array{
+        switch($Filtre) {
+            default: throw(new Exception);
+            break;
+            case tag: $requete = "SELECT * FROM CUI_RECETTE WHERE CUI_RECETTE.REC_ID IN(SELECT REC_ID FROM CUI_ETIQUETTAGE WHERE CUI_ETIQUETTAGE.TAG_ID IN (SELECT TAG_ID FROM CUI_TAG WHERE CUI_TAG.TAG_INTITULE='{$VALUE}'))";
+            break;
+            case ingredient : "SELECT * FROM CUI_RECETTE WHERE CUI_RECETTE.REC_ID IN(SELECT REC_ID FROM CUI_INGREDIENT WHERE ING_ID IN (SELECT ING_ID FROM CUI_INGREDIENT WHERE ING_INTITULE='{$VALUE}'))";
+            break;
+            case nom : "SELECT * FROM CUI_RECETTE WHERE REC_TITRE LIKE ('%{$VALUE}%')";
+        }
+        $resultat = self::projectionBdd($requete);
+        $recettes = [];
+
+        foreach($resultat as $ligne){
+            $recettes[] = new Recette($ligne['REC_ID'], $ligne['REC_TITRE'], $ligne['REC_CONTENU'], $ligne['REC_RESUME'], $ligne['REC_IMAGE'],
+                $ligne['REC_DATE_CREATION'], $ligne['REC_DATE_MODIFICATION'], $ligne['REC_STATUT'], $ligne['UTIL_ID'], $ligne['CAT_CODE']);
+        }
+
+        return $recettes;
+    }
 }
