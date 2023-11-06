31dc6743989963e688b174a2075dd514554237d3
GuilhemehliuG <46083349+freijya@users.noreply.github.com>
2023-11-06 14:03:56 +0100

    :workinprogress: wip : la page d'accueil

--- src/controllers/accueil.php
+++ src/controllers/accueil.php
@@ -3,6 +3,14 @@
 // Ceci est un contrôleur, il se charge de récupérer les données depuis le modèle pour les fournir à la vue (en gros).
 // C'est également des contrôleurs qu'on utilise pour valider les formulaires.
 
-function pageAccueil($nb){
-    require 'templates/accueil.php';
+require_once 'src/controllers/controleur.php';
+
+require_once 'src/model/recetteManager.php';
+require_once 'templates/accueil.php';
+
+class AccueilControleur extends Controleur {
+    public function executer(){
+        $recettes = (new RecetteManager)->getDernieresRecettes(3);
+        afficherPageAccueil($recettes);
+    }
 }
