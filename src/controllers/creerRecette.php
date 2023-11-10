6fbfe8df263167c0eae49621920f9619fdfb408f
Guilhem <gsaintgaudin@gmail.com>
2023-11-10 11:18:22 +0100

    :construction: wip : tried to do simpler

---
+++ src/controllers/creerRecette.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once 'src/controllers/controleur.php';
+
+require_once 'src/model/recetteManager.php';
+
+
+class creerRecette{
+
+    public function insererRecette($titre, $resume, $recette, $image, $ingredients) {
+        
+        $listeingredients = explode(',', $ingredients);
+
+        
+
+    }
+}
--- templates/creationRecetteUtilisateur.php
+++ templates/creationRecetteUtilisateur.php
@@ -1,10 +1,15 @@
 <?php
 
+include_once 'src/controllers/creationRecetteUtilisateur.php';
+
 $titre = 'nouvelle recette';
+
+$script = '<script src="assets/js/listeRecette.js"></script>';
 
 // ob_start crée un buffer qui va récupérer tout ce qui est censé être affiché (echo + HTML) 
 ob_start();
 
+$ingredients = array();
 
     ?>
 
@@ -16,7 +21,7 @@ ob_start();
                 </div>
             </div>
 
-            <form>
+            <form method="post">
 
                 <div>
                     <label for="titre">Titre :</label>
@@ -24,24 +29,48 @@ ob_start();
                 </div>
                 <div>
                     <label for="resume">Résumé :</label>
-                    <input type="text" id="resume">
+                    <textarea id="resume" style="width:100%; height:100px"></textarea>
                 </div>
                 <div>
                     <label for="recette">Recette :</label>
-                    <input type="text" id="recette">
+                    <textarea id="recette"style="width:100%; height:400px"></textarea>
                 </div>
                 <div>
-                    <input type="file" id="image">
+                    <label for="image">Lien vers l'image :</label>
+                    <input type="text" id="image">
                 </div>
+
+                <div>
+                    <label for="ingredients">ingredients (séparez les par des virgules) :</label>
+                    <input type="text" id="ingredients">
+                </div>
+
+                <button type="submit" id="boutonvalider">Enregistrer</button>
+
             </form>
 
 
 
         </div>
+        <?php
+        include_once 'assets/js/creationRecette.js'
+        ?>
 
 <?php
 
+$controleurcreationrecette = new creerRecette();
 
+if(isset($_POST['titre']) and isset($_POST['resume']) and isset($_POST['recette']) and isset($_POST['image']) and isset($_POST['ingredients'])){
+    
+    $titre = $_POST['titre'];
+    $resume = $_POST['resume'];
+    $recette = $_POST['recette'];
+    $image = $_POST['image'];
+    $ingredients = $_POST['ingredients'];
+
+    $controleurcreationrecette->insererRecette($titre, $resume, $recette, $image, $ingredients);
+
+}
 
 
 // On peut récupérer le contenu du buffer comme ça :
